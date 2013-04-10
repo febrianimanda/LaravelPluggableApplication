7c89899fee78e95166d502fb99661a311ffc94b1
Frederic CHAPLIN <fredericchaplin@gmail.com>
2013-04-10 17:45:32 +0200

    added support for SCORM .1.2 variables

--- app/controllers/LMSController.php
+++ app/controllers/LMSController.php
@@ -6,13 +6,32 @@ class LMSController extends BaseController {
 	
 	public function initialize()
 	{
-		
 		$response = Response::make('Hello World');
 		return $response;
 	}
 	
 	public function getValue($varname)
 	{
+		if ($varname == 'cmi.core._children') {
+			return "student_id,student_name,lesson_location,credit,lesson_status,entry,score,total_time,exit,session_time";
+		} else if ($varname == 'cmi.core.student_id'){
+		}else if ($varname == 'cmi.core.student_name'){
+		}else if ($varname == 'cmi.core.session_time'){
+		}else if ($varname == 'cmi.core.total_time'){
+		}else if ($varname == 'cmi.core.score._children'){
+		}else if ($varname == 'cmi.core.score.raw'){
+		}else if ($varname == 'cmi.core.lesson_location'){
+		}else if ($varname == 'cmi.core.credit'){
+		}else if ($varname == 'cmi.core.lesson_status'){
+		}else if ($varname == 'cmi.core.entry'){
+		}else if ($varname == 'cmi.core.exit'){
+		}else if ($varname == 'cmi.suspend_data'){
+		}else if ($varname == 'cmi.launch_data'){
+		}else if ($varname == 'cmi.completion_status'){
+		}else if ($varname == 'cmi.success_status'){
+		}
+			
+		
 		$result = "no result";
 		return $result;
 	}
---
+++ app/models/Session.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+class Session extends Eloquent implements ModuleInterface{
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'sessions';
+	
+}
